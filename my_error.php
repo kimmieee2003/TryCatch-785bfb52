044dfd2e7298236030d4e7558b7440c2c9648e7c
Kimberly Hogeveen <2103405@talnet.nl>
2020-04-01 11:19:16 +0200

    verbeter 2

--- my_error.php
+++ my_error.php
@@ -1,13 +1,13 @@
 <?php
 
 function countDown() {
-    $i = 0; 
+    $i = 12; 
     try {
         throw new Exception('foo');
     } catch (Exception $e) {
         if($i == 0 && $i < 11){
-            return 'Er is iets fout gegaan';  
-        }
+            return 'Er is iets goed gegaan';  
+        }else return "er is iets fout gegaan";
        
     } 
 }
